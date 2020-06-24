e7bdffe83cde79fa4ba85d94f3fd4a66107f17ab
minh.hc <minh.hc@geekup.vn>
2020-06-24 09:02:38 +0700

    update

---
+++ src/migrations/1592963206316-tag-table.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateDatabase1592963206316 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`  
+          CREATE TABLE "tagCompany" (
+            "id" SERIAL PRIMARY KEY,
+            "tag" varchar(500),
+            "id_company" integer
+          );
+          
+          CREATE TABLE "tagContact" (
+            "id" SERIAL PRIMARY KEY,
+            "tag" varchar(500),
+            "id_contact" integer
+          );
+          
+          CREATE TABLE "contact_referral" (
+            "id" SERIAL PRIMARY KEY,
+            "id_source" integer,
+            "id_target" integer
+          );
+          
+          ALTER TABLE "tagContact" ADD FOREIGN KEY ("id_contact") REFERENCES "contact" ("id");
+          
+          ALTER TABLE "tagCompany" ADD FOREIGN KEY ("id_company") REFERENCES "company" ("id");
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query('DROP TABLE "tagCompany"');
+        await queryRunner.query('DROP TABLE "tagContact"');
+    }
+}
