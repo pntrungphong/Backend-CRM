b3fa4d8001b22054d89ad126084ca7e914e991a4
Hoàng Minh <minh.hc@geekup.vn>
2020-06-24 02:04:36 +0000

    Update 1592530936900-create_database_table.ts.ts

--- src/migrations/1592530936900-create_database_table.ts.ts
+++ src/migrations/1592530936900-create_database_table.ts.ts
@@ -1,5 +1,5 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
-export class CreateDatabase1592530936900 implements MigrationInterface {
+export class CreateDatabaseTable1592530936900 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
           CREATE TABLE "contact" (
