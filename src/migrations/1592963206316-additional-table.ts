0976f7051ceae8b5dc5520b8bf998b8e1dd6b1fb
minh.hc <minh.hc@geekup.vn>
2020-06-24 15:12:40 +0700

    update

--- src/migrations/1592963206316-additional-table.ts
+++ src/migrations/1592963206316-additional-table.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateDatabase1592963206316 implements MigrationInterface {
+export class AdditionalTable1592963206316 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`  
           CREATE TABLE "tagCompany" (
@@ -14,14 +14,25 @@ export class CreateDatabase1592963206316 implements MigrationInterface {
             "tag" varchar(500),
             "id_contact" integer    
           );
+
+          CREATE TABLE "website" (
+            "id" SERIAL PRIMARY KEY,
+            "id_source" integer,
+            "type" varchar(20),
+            "name" varchar(50),    
+            "url" varchar(50)    
+          );
           
           ALTER TABLE "tagContact" ADD FOREIGN KEY ("id_contact") REFERENCES "contact" ("id");
           ALTER TABLE "tagCompany" ADD FOREIGN KEY ("id_company") REFERENCES "company" ("id");
+          ALTER TABLE "website" ADD FOREIGN KEY ("id_source") REFERENCES company ("id");
+          ALTER TABLE "website" ADD FOREIGN KEY ("id_source") REFERENCES contact ("id");
         `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query('DROP TABLE "tagCompany"');
         await queryRunner.query('DROP TABLE "tagContact"');
+        await queryRunner.query('DROP TABLE "website"');
     }
 }
--- src/migrations/1592964720304-update-column-length.ts
+++ src/migrations/1592964720304-update-column-length.ts
@@ -4,10 +4,19 @@ export class UpdateColumnLength1592964720304 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
             ALTER TABLE company
+            DROP COLUMN website;
+        `);
+
+        await queryRunner.query(`
+            ALTER TABLE contact
+            DROP COLUMN website;
+        `);
+
+        await queryRunner.query(`
+            ALTER TABLE company
             ALTER COLUMN address TYPE varchar(1000),
             ALTER COLUMN email TYPE varchar(500),
-            ALTER COLUMN phone TYPE varchar(500)
-            ALTER COLUMN website TYPE varchar(500)
+            ALTER COLUMN phone TYPE varchar(500),
             ALTER COLUMN url TYPE varchar(500)
         `);
 
@@ -16,7 +25,6 @@ export class UpdateColumnLength1592964720304 implements MigrationInterface {
             ALTER COLUMN phone TYPE varchar(500),
             ALTER COLUMN email TYPE varchar(500),
             ALTER COLUMN address TYPE varchar(1000)
-            ALTER COLUMN website TYPE varchar(500)
         `);
     }
 
