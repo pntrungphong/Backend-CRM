39b73d243dffae427d20a4f71bece41e3e5104df
minh.hc <minh.hc@geekup.vn>
2020-07-01 09:02:10 +0700

    update src

--- src/modules/contact/contactreferral.repository.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { EntityRepository } from 'typeorm/decorator/EntityRepository';
-
-import { ContactReferralEntity } from './contactreferral.entity';
-@EntityRepository(ContactReferralEntity)
-export class ContactReferralRepository extends Repository<
-    ContactReferralEntity
-> {}
