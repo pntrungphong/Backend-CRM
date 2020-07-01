d16ce891220e683ad7bbc0db081b8d5ee4887a7a
minh.hc <minh.hc@geekup.vn>
2020-07-01 08:46:27 +0700

    update lead table

--- src/migrations/1593420137250-add_lead_table.ts
+++ src/migrations/1593420137250-add_lead_table.ts
@@ -10,6 +10,7 @@ export class AddLeadTable1593420137250 implements MigrationInterface {
             "id_contact" integer ,
             "notes" text,
             "rank" integer,
+            "status" varchar(30),
             "created_by" varchar(200) NOT NULL,
             "updated_by" varchar(200) NOT NULL,
             "created_at" timestamptz NOT NULL DEFAULT now(),
@@ -17,15 +18,13 @@ export class AddLeadTable1593420137250 implements MigrationInterface {
           );
           
           CREATE TABLE "contact_lead" (
-            "contactId" integer,
-            "leadId" integer
+            "id_contact" integer,
+            "id_lead" integer
           );
           
           ALTER TABLE "lead" ADD FOREIGN KEY ("id_company") REFERENCES "company" ("id");
-
-          ALTER TABLE "contact_lead" ADD FOREIGN KEY ("contactId") REFERENCES "contact" ("id");
-          
-          ALTER TABLE "contact_lead" ADD FOREIGN KEY ("leadId") REFERENCES "lead" ("id");
+          ALTER TABLE "contact_lead" ADD FOREIGN KEY ("id_contact") REFERENCES "contact" ("id");
+          ALTER TABLE "contact_lead" ADD FOREIGN KEY ("id_lead") REFERENCES "lead" ("id");
           
         `);
     }
