081260398711804c527f6aff8e1445ce3b9469a9
guphongpham <phong.pn@geekup.vn>
2020-07-31 15:01:01 +0700

    get file touchpoint in lead detail

--- src/modules/client/repository/companyContact.repository.ts
+++
@@ -1,9 +0,0 @@
-import { Repository } from 'typeorm';
-import { EntityRepository } from 'typeorm/decorator/EntityRepository';
-
-import { CompanyContactEntity } from '../entity/companyContact.entity';
-
-@EntityRepository(CompanyContactEntity)
-export class CompanyContactRepository extends Repository<
-    CompanyContactEntity
-> {}
